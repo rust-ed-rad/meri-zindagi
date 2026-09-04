d671059fea94dd6a342895faf0286bb5b1b12843
rust-ed-rad <istiakadib15@gmail.com>
2026-09-05 02:09:43 +0600

    Update app.py

--- RecallAlbum/app.py
+++ RecallAlbum/app.py
@@ -98,4 +98,9 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000))
+)
